f1a73c95b9331066e10f515f9e486a019cedb6cb
David Steiner <david_j_steiner@yahoo.co.nz>
2022-11-06 08:50:26 +0000

    Derive equality on Messages to allow for nicer comparisons in unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,1 @@
-extern crate core;
-
 mod messages;
--- src/messages.rs
+++ src/messages.rs
@@ -1,14 +1,14 @@
-extern crate base64_serde;
 use base64::STANDARD;
 
 use base64_serde::base64_serde_type;
 use serde::{Deserialize, Serialize};
+use serde_json::Result as JsonResult;
 
 base64_serde_type!(Base64Standard, STANDARD);
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 #[serde(tag = "action")]
-enum Message {
+pub(crate) enum Message {
     #[serde(rename = "initialize")]
     Initialize(InitPayload),
     #[serde(rename = "processRecords")]
@@ -23,15 +23,15 @@ enum Message {
     ShutdownRequested(CheckpointPayload),
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 #[serde(rename_all = "camelCase")]
-struct InitPayload {
+pub(crate) struct InitPayload {
     shard_id: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 #[serde(rename_all = "camelCase")]
-struct RecordPayload {
+pub(crate) struct RecordPayload {
     #[serde(rename = "data", with = "Base64Standard")]
     raw_data: Vec<u8>,
     partition_key: String,
@@ -40,23 +40,29 @@ struct RecordPayload {
     approximate_arrival_timestamp: f64,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 #[serde(rename_all = "camelCase")]
-struct ProcessRecordPayload {
+pub(crate) struct ProcessRecordPayload {
     records: Vec<RecordPayload>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 #[serde(rename_all = "camelCase")]
-struct CheckpointPayload {
+pub(crate) struct CheckpointPayload {
     checkpoint: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 #[serde(rename_all = "camelCase")]
-struct CheckpointWithErrorPayload {
+pub(crate) struct CheckpointWithErrorPayload {
     checkpoint: String,
     error: String,
+}
+
+#[allow(dead_code)] // TODO: not used anywhere yet, remove when it's used
+pub(crate) fn parse_message(payload: &str) -> JsonResult<Message> {
+    // TODO: we should map to our own error types
+    serde_json::from_str::<Message>(payload)
 }
 
 #[cfg(test)]
@@ -66,12 +72,12 @@ mod tests {
     #[test]
     fn parse_initialize() {
         let given = "{\"action\" :\"initialize\", \"shardId\": \"shard1\"}";
-        let parsed = serde_json::from_str::<Message>(given);
+        let expected = Message::Initialize(InitPayload {
+            shard_id: "shard1".to_string(),
+        });
 
-        match parsed.unwrap() {
-            Message::Initialize(InitPayload { shard_id }) => assert_eq!(shard_id, "shard1"),
-            _ => panic!("Did not match expected Initialize event."),
-        }
+        let actual = parse_message(given).unwrap();
+        assert_eq!(actual, expected);
     }
 
     #[test]
@@ -82,69 +88,65 @@ mod tests {
             \"partitionKey\": \"1\",\
             \"sequenceNumber\": \"49590338271490256608559692538361571095921575989136588898\",\
             \"approximateArrivalTimestamp\": 1570887011763.01}]}";
-        let parsed = serde_json::from_str::<Message>(given);
+        let parsed = parse_message(given).unwrap();
 
-        match parsed.unwrap() {
-            Message::ProcessRecords(ProcessRecordPayload { records }) => {
-                assert_eq!(records.len(), 1);
-                assert_eq!(
-                    records[0].sequence_number,
-                    "49590338271490256608559692538361571095921575989136588898"
-                );
-                assert_eq!(records[0].approximate_arrival_timestamp, 1570887011763.01);
-                // TODO check if we can do this in serde
-                assert_eq!(
-                    "Hello, this is a test.",
-                    String::from_utf8_lossy(&records[0].raw_data)
-                );
-            }
-            _ => panic!("Did not match expected ProcessRecords event."),
+        if let Message::ProcessRecords(ProcessRecordPayload { records }) = parsed {
+            assert_eq!(records.len(), 1);
+            assert_eq!(
+                records[0].sequence_number,
+                "49590338271490256608559692538361571095921575989136588898"
+            );
+            assert_eq!(records[0].approximate_arrival_timestamp, 1570887011763.01);
+            // TODO check if we can do this in serde
+            assert_eq!(
+                "Hello, this is a test.",
+                String::from_utf8_lossy(&records[0].raw_data)
+            );
+        } else {
+            panic!("Did not match expected ProcessRecords event.");
         }
     }
 
     #[test]
     fn parse_checkpoint() {
         let given = "{\"action\": \"checkpoint\", \"checkpoint\": \"1234\", \"error\": \"check your point\"}";
-        let parsed = serde_json::from_str::<Message>(given);
+        let expected = Message::Checkpoint(CheckpointWithErrorPayload {
+            checkpoint: "1234".to_string(),
+            error: "check your point".to_string(),
+        });
 
-        match parsed.unwrap() {
-            Message::Checkpoint(CheckpointWithErrorPayload { checkpoint, error }) => {
-                assert_eq!(checkpoint, "1234");
-                assert_eq!(error, "check your point");
-            }
-            _ => panic!("Did not match expected Checkpoint event."),
-        }
+        let actual = parse_message(given).unwrap();
+        assert_eq!(actual, expected);
     }
 
     #[test]
     fn parse_lease_lost() {
         let given = "{\"action\": \"leaseLost\"}";
-        let parsed = serde_json::from_str::<Message>(given);
-        match parsed.unwrap() {
-            Message::LeaseLost => {}
-            _ => panic!("Did not match expected LeaseLost event."),
-        }
+        let expected = Message::LeaseLost;
+
+        let actual = parse_message(given).unwrap();
+        assert_eq!(actual, expected)
     }
 
     #[test]
     fn parse_shard_end() {
         let given = "{\"action\": \"shardEnded\", \"checkpoint\": \"1234\"}";
-        let parsed = serde_json::from_str::<Message>(given);
-        match parsed.unwrap() {
-            Message::ShardEnded(CheckpointPayload { checkpoint }) => assert_eq!(checkpoint, "1234"),
-            _ => panic!("Did not match expected ShardEnded event."),
-        }
+        let expected = Message::ShardEnded(CheckpointPayload {
+            checkpoint: "1234".to_string(),
+        });
+
+        let actual = parse_message(given).unwrap();
+        assert_eq!(actual, expected);
     }
 
     #[test]
     fn parse_shutdown_requested() {
         let given = "{\"action\": \"shutdownRequested\", \"checkpoint\": \"1234\"}";
-        let parsed = serde_json::from_str::<Message>(given);
-        match parsed.unwrap() {
-            Message::ShutdownRequested(CheckpointPayload { checkpoint }) => {
-                assert_eq!(checkpoint, "1234")
-            }
-            _ => panic!("Did not match expected ShardEnded event."),
-        }
+        let expected = Message::ShutdownRequested(CheckpointPayload {
+            checkpoint: "1234".to_string(),
+        });
+
+        let actual = parse_message(given).unwrap();
+        assert_eq!(actual, expected)
     }
 }
