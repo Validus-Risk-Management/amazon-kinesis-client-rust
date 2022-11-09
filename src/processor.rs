64273ea8c2f1d443a65574b94f2d6ce1466e48f1
Fergus Dixon <fergus.dixon@validusrm.com>
2022-11-09 13:49:43 +0000

    add stubs for missing message types

--- src/processor.rs
+++ src/processor.rs
@@ -3,4 +3,7 @@ use crate::messages::Record;
 pub trait Processor {
     fn initialize(&mut self, shard_id: &str);
     fn process_records(&mut self, data: &[Record]);
+    fn lease_lost(&mut self);
+    fn shard_ended(&mut self);
+    fn shutdown_requested(&mut self);
 }
